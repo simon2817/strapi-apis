1992c4ec20bbce44668a52620f8de7f30e5fa4e0
simon2817 <trillos2000@gmail.com>
2020-11-14 19:21:57 -0500

    Inicio proyecto creacion de apis strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2db9cc800ce0fb556c8b569e3252cd90'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dbf9be4f-8039-49a4-af7d-92b95ea847b4'
+};
